e91eb86ab26498f1513d94dcd953fa1b13f22af3
riswanvp <riswanvpofficial@gmail.com>
2025-05-19 23:44:09 +0530

    updated

--- main.tf
+++ main.tf
@@ -40,6 +40,17 @@ resource "aws_security_group_rule" "inbound" {
   to_port           = var.open-ports[count.index]
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+##Enable security inbound rule using for loop
+##resource "aws_security_group_rule" "inbound_main" {
+##  for_each = toset(var.open-ports)
+##  type = "ingress"
+## security_group_id = aws_security_group.allow-all.id
+##  from_port = each.key
+##  to_port = each.key
+##  protocol = "tcp"
+##  cidr_blocks = ["0.0.0.0/0"]
+#}
 resource "aws_security_group_rule" "outbound" {
   type              = "egress"
   from_port         = "0"
